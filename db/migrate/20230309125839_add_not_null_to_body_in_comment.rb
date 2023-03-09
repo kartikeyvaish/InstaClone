6c55222f97a544fa233be57fdef8c53933600383
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-03-09 18:29:02 +0530

    added comment body not NULL constraint

---
+++ db/migrate/20230309125839_add_not_null_to_body_in_comment.rb
@@ -0,0 +1,5 @@
+class AddNotNullToBodyInComment < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :comments, :body, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_115847) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_125839) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.bigint "post_id", null: false
     t.bigint "user_id", null: false
     t.boolean "is_edited", default: false
