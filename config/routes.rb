4937630c3fa86f5a8b9a75fcd914da42b757ad69
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-03-09 16:37:48 +0530

    added likes routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
   get "/posts/:id", to: "post#show"
   delete "/posts/:id", to: "post#delete"
 
+  # Likes Routes
+  get "/likes/post/:id", to: "like#get_likes_on_post"
+  post "/likes/post/:id", to: "like#like_a_post"
+  delete "/likes/post/:id", to: "like#unlike_a_post"
+
   # Tokens
   get "/token/refresh", to: "refresh_token#refresh_token"
 end
